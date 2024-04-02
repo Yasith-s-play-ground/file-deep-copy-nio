2089ebad7342b605c1c260f237bcdd797edcc4e0
Yasith Ishara Perera <yasithishara@gmail.com>
2024-04-02 10:16:07 +0530

    after adding progress bar but not working correctly

--- src/main/java/lk/ijse/dep12/se/io/controller/MainViewController.java
+++ src/main/java/lk/ijse/dep12/se/io/controller/MainViewController.java
@@ -27,6 +27,12 @@ public class MainViewController {
     public ProgressBar pgbCopy;
     private long totalSize;
     private long copiedSize = 0;
+    // private Task copyTask = null;
+    private Thread fileCopyThread = null;
+
+    public void initialize() {
+        reset();
+    }
 
     public void btnSourceDirectoryOnAction(ActionEvent actionEvent) {
         DirectoryChooser directoryChooser = new DirectoryChooser();
@@ -56,37 +62,93 @@ public class MainViewController {
         Path source = Paths.get(txtSource.getText());
         Path target = Paths.get(txtTarget.getText());
         totalSize = Files.size(source);
-
         copyFiles(source, target);
 
+//        copyTask = new Task<>() {
+//            @Override
+//            protected Object call() throws Exception {
+//                return null;
+//            }
+//        };
+//        copyTask.progressProperty().addListener((observable, previous, current) -> {
+//            double progress = current.doubleValue() * 100;
+//            lblProgress.setText("%.2f".formatted(progress) + "%");
+//        });
+//        lblStatus.textProperty().bind(copyTask.messageProperty());
+//        pgbCopy.progressProperty().bind(copyTask.progressProperty());
+//
+//        new Thread(copyTask).start();
 
+        //lblStatus.setText("Copying complete !!");
+//                            updateMessage("Copying " + path.getFileName());
+//                            updateProgress((double) copiedSize / totalSize * 100, 100);
+        // pgbCopy.progressProperty().setValue(1);
+        // lblProgress.setText("100.00%");
     }
 
     public void btnResetOnAction(ActionEvent actionEvent) {
+        reset();
     }
 
 
     private void copyFiles(Path source, Path target) throws IOException {
-
-
-        if (Files.notExists(target)) {
-            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING); // copy the folder ( empty )
-        }
-        for (Path path : Files.newDirectoryStream(source)) {
-            if (Files.notExists(target.resolve(path.getFileName()))) { //check whether this folder is already copied
-                if (Files.isDirectory(path)) {
-                    //if this is a directory, going inside it until only files are available
-                    copyFiles(path, target.resolve(path.getFileName()));
-                } else {
-                    //copy items which are not directories
-                    copiedSize += Files.size(path);
-                    Files.copy(path, target.resolve(path.getFileName()), StandardCopyOption.REPLACE_EXISTING); // copy everything into the target folder
+        Task timeConsumingTask = new Task<>() {
+            @Override
+            protected Object call() throws Exception {
+                if (Files.notExists(target)) {
+                    //lblStatus.setText("Copying " + source.getFileName());
+                    updateMessage("Copying " + source.getFileName());
+                    Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING); // copy the folder ( empty )
                 }
+                for (Path path : Files.newDirectoryStream(source)) {
+                    if (Files.notExists(target.resolve(path.getFileName()))) { //check whether this folder is already copied
+                        if (Files.isDirectory(path)) {
+                            //if this is a directory, going inside it until only files are available
+                            copyFiles(path, target.resolve(path.getFileName()));
+                        } else {
+                            //copy items which are not directories
+                            copiedSize += Files.size(path);
+                            //lblStatus.setText("Copying " + path.getFileName());
+                            updateMessage("Copying " + path.getFileName());
+                            updateProgress((double) copiedSize / totalSize * 100, 100);
+                            //pgbCopy.progressProperty().setValue((double) copiedSize / totalSize);
+                            //lblProgress.setText("%.2f".formatted((double) copiedSize / totalSize) + "%");
+                            Files.copy(path, target.resolve(path.getFileName()), StandardCopyOption.REPLACE_EXISTING); // copy everything into the target folder
+                        }
+                    }
+                }
+                updateMessage("Installation completed. Click to reset again!");
+                return null;
             }
-        }
+        };
+        timeConsumingTask.progressProperty().addListener((observable, previous, current) -> {
+            double progress = current.doubleValue() * 100;
+            lblProgress.setText("%.2f".formatted(progress) + "%");
+        });
+        lblStatus.textProperty().bind(timeConsumingTask.messageProperty());
+        pgbCopy.progressProperty().bind(timeConsumingTask.progressProperty());
+
+        //if thread is not initialized, create and start new thread
+//        if (fileCopyThread == null) {
+//            System.out.println("thread is not initialized");
+//            fileCopyThread = new Thread(timeConsumingTask);
+//            fileCopyThread.start();
+//        }
+
+        new Thread(timeConsumingTask).start();
+
     }
 
     private void enableCopyButton() {
         btnCopy.setDisable(txtSource.getText().isEmpty() || txtTarget.getText().isEmpty());
+    }
+
+    private void reset() {
+        txtTarget.clear();
+        txtSource.clear();
+        enableCopyButton();
+        pgbCopy.progressProperty().setValue(0);
+        lblStatus.setText("");
+        lblProgress.setText("");
     }
 }
